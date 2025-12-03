c213c311da58a62bd8c976140740a2491275ba99
MAGAM VENKATA DHANUSH <vvenkatadhanush36@gmail.com>
2025-12-03 17:44:46 +0530

    Add files via upload

---
+++ area of square.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float side,area;
+	printf("enter the side of square:");
+	scanf("%f",&side);
+	area = side*side;
+	printf("area of square = %f\n",area);
+	return 0;
+}
