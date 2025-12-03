966983c097ddfbf7acd8b41d4a8495b83f641355
MAGAM VENKATA DHANUSH <vvenkatadhanush36@gmail.com>
2025-12-03 17:44:18 +0530

    Add files via upload

---
+++ araay.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,a[10];
+	printf("enter n value");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=n-1;i>=0;i--)
+	{
+		printf("%d",&a[i]);
+	}
+}
