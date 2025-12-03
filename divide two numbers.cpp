47db82e8828f0d7159e8b9e14ee613051e4e0d6f
MAGAM VENKATA DHANUSH <vvenkatadhanush36@gmail.com>
2025-12-03 17:53:07 +0530

    Add files via upload

---
+++ divide two numbers.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float num1,num2,div;
+	printf("enter the num1");
+	scanf("%f",&num1);
+	printf("enter the num2");
+	scanf("%f",&num2);
+	div = num1/num2;
+	printf("div =%f\n",div);
+	return 0;
+}
