476d6b1266cc1ba2294bcec82b45ce97884f6c85
MAGAM VENKATA DHANUSH <vvenkatadhanush36@gmail.com>
2025-12-03 17:45:14 +0530

    Add files via upload

---
+++ array(transpose of matrix).cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,a[3][3],t[3][3];
+	printf("enter array elements :");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			t[i][j]=a[j][i];
+		}
+    }
+    printf("transpose of matrix is :");
+    for(i=0;i<3;i++)
+    {
+    	for(j=0;j<3;j++)
+    	{
+    		printf("%d\t",t[i][j]);
+		}
+		printf("\n");
+	}
+	
+    
+}
